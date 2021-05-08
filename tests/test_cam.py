9342e1f9552ee983375555c6b6e0f4f2daa0906f
Bahadir Arslan <arslan.bah@gmail.com>
2021-05-08 11:15:23 +0200

    Reworked tests.

--- tests/test_cam.py
+++ tests/test_cam.py
@@ -1,18 +1,32 @@
+#!../interpreter/bin/python
+
+"""
+This module tests the camera if it can take photos.
+"""
+
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
 import cv2
 import time
 
+
+print("Testing camera...")
+
 camera = PiCamera()
 camera.rotation = 180
-
+camera.resolution = (640, 480)
 raw_capture = PiRGBArray(camera)
 
-time.sleep(0.1)
+
+# Let camera warm up.
+time.sleep(1)
 
 camera.capture(raw_capture, format="bgr")
 image = raw_capture.array
+
+print("You should now see a photo.")
 
 cv2.imshow("image", image)
 cv2.waitKey(0)
---
+++ tests/test_cam_vid.py
@@ -0,0 +1,40 @@
+#!../interpreter/bin/python
+
+"""
+This module tests the camera if it can take videos.
+"""
+
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+import cv2
+import time
+
+
+print("Testing camera...")
+
+resolution = (640, 640)
+camera = PiCamera(resolution=resolution)
+camera.rotation = 180
+
+raw_capture = PiRGBArray(camera, size=resolution)
+
+# Let camera warm up.
+time.sleep(1)
+
+print("You should now see a live video.")
+
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+    image = frame.array
+
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    raw_capture.truncate(0)
+
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
