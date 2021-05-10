ddcc91e9b9fbebda04a2964ffd0fc4e71c625f3d
Bahadir Arslan <arslan.bah@gmail.com>
2021-05-10 21:07:06 +0200

    Extended combiner.

--- combine_photos.py
+++ combine_photos.py
@@ -6,31 +6,54 @@ import cv2
 from cv2 import VideoWriter, VideoWriter_fourcc
 import numpy as np
 
+EXTENSION = ".png"
+
+MULTIPLE_DIMENSIONS_ERROR_MESSAGE = "Multiple dimensions in the same sequence is not allowed!"
+
+
 parser = argparse.ArgumentParser(description="Makes timelapse video from the photos in the given folder.")
-parser.add_argument("--path", "-P", type=str)
-parser.add_argument("--width", "-W", type=int, default=800)
-parser.add_argument("--height", "-H", type=int, default=800)
+parser.add_argument("--path", "-P", type=str, required=True)
+parser.add_argument("--output_file_name", "-O", type=str, required=True)
+parser.add_argument("--dimensions", "-D", type=int, default=0)
 parser.add_argument("--frame_per_second", "-FPS", type=int, default=24)
 parser.add_argument("--frame_time", "-FT", type=int, default=1)
 
-
 args = parser.parse_args()
+
 
 # Get all images.
 images_folder = Path(args.path)
 if not images_folder.is_dir():
     raise Exception("A valid folder is required.")
 
-all_images = images_folder.glob("*.png")
 
-# Start video buffer.
-fourcc = VideoWriter_fourcc(*'MP42')
-video = VideoWriter('./output.avi', fourcc, float(8), (args.width, args.height))
+dimensions = args.dimensions
+width, height = None, None  # While gathering image data we also check if all of the iamges are same size.
+                            # But we only do this if dimensions are not given. If dimensions are already given
+                            # we scale the images instead.
 
-for image in sorted(all_images):
+# Collect all of the image data.
+image_data_array = []
+for image in sorted(images_folder.glob(f"*{EXTENSION}")):
+    frame_data = cv2.imread(str(image))
+    if not dimensions:
+        height_, width_ = frame_data.shape[:2]
+        if width:
+            assert width == width_, MULTIPLE_DIMENSIONS_ERROR_MESSAGE
+        if height:
+            assert height == height_, MULTIPLE_DIMENSIONS_ERROR_MESSAGE
+        width = width_
+        height = height_
 
-    # Read image data.
-    frame_data = cv2.imread(str(image), cv2.IMREAD_UNCHANGED)
+    dimensions = (width, height)
+    image_data_array.append(frame_data)
+
+print("Collected image data, writing video...")
+
+# Write video.
+video = cv2.VideoWriter(args.output_file_name, cv2.VideoWriter_fourcc(*'MP42'), float(8), dimensions)
+for frame_data in image_data_array:
     video.write(frame_data)
 
 video.release()
+print(f"Video was successfilly saved as:\n\t{args.output_file_name}")
