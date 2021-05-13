183ef9e10b4802609d28e52e94d47a667bb4d2ec
Bahadir Arslan <arslan.bah@gmail.com>
2021-05-13 14:07:04 +0200

    Reworked on the module.

--- tests/test_cam.py
+++ tests/test_cam.py
@@ -4,42 +4,39 @@
 This module tests the camera if it can take photos.
 """
 
+import time
 
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from fractions import Fraction
 
 import cv2
-import time
 
 
 print("Testing camera...")
 
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 1024
+
 camera = PiCamera()
-camera.rotation = 180
-camera.resolution = (640, 480)
+# camera.rotation = 180
+
+camera.resolution = (SCREEN_WIDTH, SCREEN_HEIGHT)
 raw_capture = PiRGBArray(camera)
 
+# Set ISO to the desired value
+# camera.iso = 800
 
 # Let camera warm up.
-
-# Set ISO to the desired value
-camera.iso = 800
-# Wait for the automatic gain control to settle
-time.sleep(5)
-# Now fix the values
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
+time.sleep(2)
 
 camera.capture(raw_capture, format="bgr")
 image = raw_capture.array
 
 print("You should now see a photo.")
 
-cv2.imshow("image", image)
+cv2.imshow("Image", image)
+# cv2.imwrite("test_image.png", image)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
