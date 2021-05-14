f9a31e1541f7e203dcfb5cdcdc1afbc9c91b8b47
Bahadir Arslan <arslan.bah@gmail.com>
2021-05-14 17:20:46 +0200

    Changed interval.

--- __main__.py
+++ __main__.py
@@ -7,5 +7,5 @@ from time_lapse_worker import TimeLapseWorker
 if __name__ == "__main__":
 
     target_folder = Path(__file__).parent / "140521"
-    time_lapse_worker = TimeLapseWorker(60*30, target_folder, 1000)
+    time_lapse_worker = TimeLapseWorker(60*5, target_folder, 1000)
     time_lapse_worker.start()
